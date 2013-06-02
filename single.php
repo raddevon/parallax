c9962e67983734db6fd7b0bf3083ffe0d3a0f329
Devon Campbell <raddevon@gmail.com>
2013-06-02 16:27:28 -0400

    Removed icons for a cleaner look

--- single.php
+++ single.php
@@ -14,7 +14,7 @@
 
 									<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 									<div class="byline vcard"><?php
-										printf(__('<p class="date"><i class="icon-calendar"></i> <time class="updated" datetime="%1$s" pubdate>%2$s</time></p> <p class="categories"><i class="icon-folder"></i> %4$s</p>', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_template_directory_uri(), get_the_category_list(', '));
+										printf(__('<p class="date"><time class="updated" datetime="%1$s" pubdate>%2$s</time></p>', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')));
 									?></div>
 
 								</header> <!-- end article header -->
